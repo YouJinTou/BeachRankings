c6fdb5a1c20ebd3ee1ad04768c527581fa7aa17a
Dimitar Dimitrov <dandreevd@gmail.com>
2016-10-11 16:02:27 +0300

    Added the BeachBindingModels.cs file.

---
+++ App/Models/BindingModels/BeachBindingModels.cs
@@ -0,0 +1,27 @@
+﻿namespace App.Models.BindingModels
+{
+    using BeachRankings.Models;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class AddBeachBindingModel
+    {
+        [Required(ErrorMessage = "The name field is required.")]
+        [MinLength(2, ErrorMessage = "The name should be at least 2 characters long.")]
+        [MaxLength(100, ErrorMessage = "The name cannot be longer than 100 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The location field is required.")]
+        [MinLength(2, ErrorMessage = "The location name should be at least 2 characters long.")]
+        [MaxLength(100, ErrorMessage = "The location name cannot be longer than 100 characters.")]
+        public string Location { get; set; }
+
+        [MaxLength(350, ErrorMessage = "The description cannot be longer than 350 characters.")]
+        public string Description { get; set; }
+        
+        [Required(ErrorMessage = "Coordinates are required. Select a beach on the map.")]
+        public string Coordinates { get; set; }
+
+        public IEnumerable<BeachPhoto> Photos { get; set; }
+    }
+}
