9a38a4d0c6affe00d97ccf92d486b45cc6ff7e3e
Dimitar Dimitrov <dandreevd@gmail.com>
2016-10-19 15:47:24 +0300

    Added the PostReviewViewModel and its mappings.

--- App/App_Start/MapperConfiguration.cs
+++ App/App_Start/MapperConfiguration.cs
@@ -26,9 +26,16 @@
                 cfg.CreateMap<AddBeachBindingModel, Beach>();
                 cfg.CreateMap<Beach, AutocompleteMainViewModel>();
 
+                cfg.CreateMap<BeachPhoto, string>().ConvertUsing(bp => bp.Path);
+
                 cfg.CreateMap<Review, ConciseReviewViewModel>()
                     .ForMember(vm => vm.UserName, model => model.MapFrom(m => m.Author.UserName))
                     .ForMember(vm => vm.AvatarPath, model => model.MapFrom(m => m.Author.AvatarPath));
+                cfg.CreateMap<Review, PostReviewViewModel>()
+                    .ForMember(vm => vm.BeachImagePaths, model => model.MapFrom(m => m.Beach.Photos))
+                    .ForMember(vm => vm.BeachName, model => model.MapFrom(m => m.Beach.Name))
+                    .ForMember(vm => vm.BeachTotalScore, model => model.MapFrom(m => m.Beach.TotalScore));
+
                 cfg.CreateMap<PostReviewBindingModel, Review>();
                 cfg.CreateMap<Review, EditReviewViewModel>();
                 cfg.CreateMap<EditReviewBindingModel, Review>().AfterMap((vm, model) => model.UpdateTotalScore());
--- App/Models/ViewModels/ReviewViewModels.cs
+++ App/Models/ViewModels/ReviewViewModels.cs
@@ -1,6 +1,7 @@
 ﻿namespace BeachRankings.App.Models.ViewModels
 {
     using System;
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
 
     public class ConciseReviewViewModel
@@ -94,6 +95,81 @@
         public double? LongTermStay { get; private set; }
 
         #endregion        
+    }
+
+    public class PostReviewViewModel
+    {
+        public IEnumerable<string> BeachImagePaths { get; set; }
+
+        public string BeachName { get; set; }
+
+        public double? BeachTotalScore { get; set; }
+
+        [Required(ErrorMessage = "The review field is required.")]
+        [MinLength(100, ErrorMessage = "100 characters should be doable.")]
+        [MaxLength(3000, ErrorMessage = "We'll happily accept 3000 symbols and below.")]
+        [Display(Name = "Review")]
+        public string Content { get; private set; }
+
+        #region Beachline
+
+        [Display(Name = "Sand quality")]
+        public double? SandQuality { get; private set; }
+
+        [Display(Name = "Beach cleanliness")]
+        public double? BeachCleanliness { get; private set; }
+
+        [Display(Name = "Beautiful scenery")]
+        public double? BeautifulScenery { get; private set; }
+
+        [Display(Name = "Crowd-free")]
+        public double? CrowdFree { get; private set; }
+
+        #endregion
+
+        #region Water
+
+        [Display(Name = "Water purity")]
+        public double? WaterPurity { get; private set; }
+
+        [Display(Name = "Waste-free seabed")]
+        public double? WasteFreeSeabed { get; private set; }
+
+        [Display(Name = "Feet-friendly bottom")]
+        public double? FeetFriendlyBottom { get; private set; }
+
+        [Display(Name = "Sea life diversity")]
+        public double? SeaLifeDiversity { get; private set; }
+
+        [Display(Name = "Coral reef wow factor")]
+        public double? CoralReef { get; private set; }
+
+        #endregion
+
+        #region Activities
+
+        [Display(Name = "Taking a walk")]
+        public double? Walking { get; private set; }
+
+        [Display(Name = "Snorkeling")]
+        public double? Snorkeling { get; private set; }
+
+        [Display(Name = "Kayaking")]
+        public double? Kayaking { get; private set; }
+
+        [Display(Name = "Camping")]
+        public double? Camping { get; private set; }
+
+        #endregion
+
+        #region Tourist Infrastructure
+
+        [Display(Name = "Environment-friendly infrastructure")]
+        public double? Infrastructure { get; private set; }
+
+        public double? LongTermStay { get; private set; }
+
+        #endregion
     }
 
     public class EditReviewViewModel
