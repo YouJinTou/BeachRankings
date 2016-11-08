ebe77defe9aa894a607b89b4c9ed26b13c699f70
Dimitar Dimitrov <dandreevd@gmail.com>
2016-11-08 20:04:06 +0200

    Improved the beach details view.

---
+++ App/Scripts/Internal/beach-details.js
@@ -0,0 +1,9 @@
+﻿(function ($) {
+    $('.slick-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        mobileFirst: true
+    });
+})(jQuery);
