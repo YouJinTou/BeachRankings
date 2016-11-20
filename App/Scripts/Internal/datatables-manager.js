03628cee96598b555422444f4f79fcec2eb229a2
Dimitar Dimitrov <dandreevd@gmail.com>
2016-11-20 11:10:31 +0200

    Criteria columns can now be hidden and the total score recalculated.

--- App/Scripts/Internal/datatables-manager.js
+++ App/Scripts/Internal/datatables-manager.js
@@ -1,6 +1,7 @@
 ﻿var DataTablesManager = function () {
     function initializeDataTable() {
         var lastFixedColumn = 2;
+        var visibilityHandlerAlreadyAttached = false;
         var table = $('#table-result').DataTable({
             scrollX: true,
             scrollY: 300,
@@ -11,7 +12,15 @@
             columnDefs: [
                 { 'width': '20px', 'targets': [0, 1, 2] }
             ],
-            autoWidth: false
+            autoWidth: false,
+            dom: 'lBfrtip',
+            buttons: [
+                {
+                    extend: 'colvis',
+                    columns: [':nth-last-child(-n+15)'],
+                    text: 'Add/remove criteria'
+                }
+            ]
         });
 
         table.columns().every(function (col) {
@@ -28,6 +37,42 @@
 
             $('.DTFC_LeftHeadWrapper [data-filter="' + filter + '"').on('click', function (event) {
                 event.stopImmediatePropagation();
+            });
+        });
+
+        $('.buttons-colvis').on('click', function () {
+            if (visibilityHandlerAlreadyAttached) {
+                return;
+            }
+
+            visibilityHandlerAlreadyAttached = true;
+
+            $('.buttons-columnVisibility').on('click', function () {
+                var firstCriterionColIndex = 8;
+                var lastColIndex = 22;
+                var totalScoreColIndex = 7;
+
+                table.rows().every(function (row) {                    
+                    var totalScore = 0;
+                    var nonSkippedCount = 0;
+
+                    for (var col = firstCriterionColIndex; col <= lastColIndex; col++) {
+                        var skipColumn = (!table.column(col).visible() || table.cell(row, col).data().length === 0);
+                        
+                        if (skipColumn) {
+                            continue;
+                        }
+
+                        totalScore += parseFloat(table.cell(row, col).data())
+                        nonSkippedCount++;
+                    }
+
+                    totalScore = (Math.round(((totalScore / nonSkippedCount) * 10)) / 10).toFixed(1);
+
+                    table.cell(row, totalScoreColIndex).data(totalScore);
+                });
+
+                table.draw();
             });
         });
     }
