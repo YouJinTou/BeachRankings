ad97be7fa1d9a79bca7ab23e1027c4dd4c02a7f8
YouJinTou <dandreevd@gmail.com>
2019-08-05 23:12:21 +0300

    Made 'table' protected.

--- BeachRankings.Core/DAL/DynamoRepository.cs
+++ BeachRankings.Core/DAL/DynamoRepository.cs
@@ -11,39 +11,33 @@ namespace BeachRankings.Core.DAL
 {
     internal class DynamoRepository<T> : IRepository<T> where T : IDbModel
     {
-        private readonly IAmazonDynamoDB dynamoDb;
-        private readonly string table;
+        protected readonly Table table;
 
         public DynamoRepository(IAmazonDynamoDB dynamoDb, string table)
         {
-            this.dynamoDb = dynamoDb;
-            this.table = table;
+            this.table = Table.LoadTable(dynamoDb, table);
         }
 
         public async Task<T> GetAsync(object id)
         {
             InputValidator.ThrowIfNull(id);
 
-            var table = Table.LoadTable(this.dynamoDb, this.table);
             var partitionKey = id.ToString();
             var sortKey = partitionKey.Replace(Constants.DynamoPartitionKeyPrefix, string.Empty);
             var document = 
-                await table.GetItemAsync(new Primitive(partitionKey), new Primitive(sortKey));
+                await this.table.GetItemAsync(new Primitive(partitionKey), new Primitive(sortKey));
 
             return document.ConvertTo<T>();
         }
 
         public async Task AddAsync(T item)
         {
-            var table = Table.LoadTable(this.dynamoDb, this.table);
-
-            await table.PutItemAsync(item.ToDynamoDbDocument());
+            await this.table.PutItemAsync(item.ToDynamoDbDocument());
         }
 
         public async Task AddManyAsync(
             IEnumerable<T> items, int batchSize = 100, int coolDown = 1000)
         {
-            var table = Table.LoadTable(this.dynamoDb, this.table);
             var page = 0;
 
             while (true)
@@ -55,7 +49,7 @@ namespace BeachRankings.Core.DAL
                     return;
                 }
 
-                var batch = table.CreateBatchWrite();
+                var batch = this.table.CreateBatchWrite();
 
                 foreach (var item in itemsBatch)
                 {
