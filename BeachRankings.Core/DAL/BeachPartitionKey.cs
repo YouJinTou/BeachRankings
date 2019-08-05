dcf6911eb7f2a514c9bec70c0b1bc241c84ed634
YouJinTou <dandreevd@gmail.com>
2019-08-05 22:17:01 +0300

    Added BeachPartitionKey.

---
+++ BeachRankings.Core/DAL/BeachPartitionKey.cs
@@ -0,0 +1,13 @@
+﻿namespace BeachRankings.Core.DAL
+{
+    public enum BeachPartitionKey
+    {
+        Continent,
+        Country,
+        L1,
+        L2,
+        L3,
+        L4,
+        WaterBody
+    }
+}
