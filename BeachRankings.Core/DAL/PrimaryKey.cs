89b2d47237b0708fc7a6aa944beb0c5aecb9942c
Dimitar Dimitrov <dandreevd@gmail.com>
2019-08-06 11:52:23 +0300

    Added PrimaryKey.

---
+++ BeachRankings.Core/DAL/PrimaryKey.cs
@@ -0,0 +1,17 @@
+﻿using BeachRankings.Core.Tools;
+
+namespace BeachRankings.Core.DAL
+{
+    public class PrimaryKey
+    {
+        public PrimaryKey(string partitionKey, string sortKey)
+        {
+            this.PartitionKey = InputValidator.ReturnOrThrowIfNullOrWhiteSpace(partitionKey);
+            this.SortKey = InputValidator.ReturnOrThrowIfNullOrWhiteSpace(sortKey);
+        }
+
+        public string PartitionKey { get; }
+
+        public string SortKey { get; }
+    }
+}
