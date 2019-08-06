8853825101a6a300b5f2e8fc14baace7541ef41f
Dimitar Dimitrov <dandreevd@gmail.com>
2019-08-06 11:52:29 +0300

    Added DalObjectsFactory.

---
+++ BeachRankings.Core/Factories/DalObjectsFactory.cs
@@ -0,0 +1,12 @@
+﻿using BeachRankings.Core.DAL;
+
+namespace BeachRankings.Core.Factories
+{
+    public static class DalObjectsFactory
+    {
+        public static PrimaryKey CreatePrimaryKey(string partitionKey, string sortKey)
+        {
+            return new PrimaryKey(partitionKey, sortKey);
+        }
+    }
+}
