a0ddd2df9bb48f6e9be5db72c563e76a2e898640
Dimitar Dimitrov <dandreevd@gmail.com>
2019-08-06 13:05:30 +0300

    Added more properties.

--- BeachRankings.Core/Models/BeachQueryModel.cs
+++ BeachRankings.Core/Models/BeachQueryModel.cs
@@ -3,5 +3,23 @@
     public class BeachQueryModel
     {
         public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Continent { get; set; }
+
+        public string Country { get; set; }
+
+        public string L1 { get; set; }
+
+        public string L2 { get; set; }
+
+        public string L3 { get; set; }
+
+        public string L4 { get; set; }
+
+        public string WaterBody { get; set; }
+
+        public string Coordinates { get; set; }
     }
 }
