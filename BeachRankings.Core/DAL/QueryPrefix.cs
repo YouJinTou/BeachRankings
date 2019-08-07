b4d7746f8862db144df666e06f7d2f7c9104b6c8
YouJinTou <dandreevd@gmail.com>
2019-08-07 22:11:41 +0300

    Added QueryPrefix.

---
+++ BeachRankings.Core/DAL/QueryPrefix.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BeachRankings.Core.DAL
+{
+    internal class QueryPrefix
+    {
+        public QueryPrefix(string prefix)
+        {
+            this.OriginalValue = prefix;
+            this.CleanValue = this.GetCleanValue(prefix);
+        }
+
+        public string OriginalValue { get; }
+
+        public string CleanValue { get; }
+
+        public static implicit operator string(QueryPrefix prefix)
+        {
+            return prefix.CleanValue;
+        }
+
+        private string GetCleanValue(string prefix)
+        {
+            var pattern = "[^a-zA-Z0-9.-]";
+
+            return Regex.Replace(prefix, pattern, string.Empty).ToLower();
+        }
+    }
+}
