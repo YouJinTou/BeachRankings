6d6a450c455107182a455e65cbcdb23477c45867
YouJinTou <dandreevd@gmail.com>
2019-08-07 20:36:42 +0300

    Added TypeExtensions.

---
+++ BeachRankings.Core/Extensions/TypeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using BeachRankings.Core.Tools;
+using System;
+using System.Reflection;
+
+namespace BeachRankings.Core.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static PropertyInfo TryGetProperty(
+            this Type type, string propertyName, string fallbackProperty)
+        {
+            var property = type.GetProperty(propertyName);
+
+            if (property == null)
+            {
+                InputValidator.ThrowIfNullOrWhiteSpace(fallbackProperty);
+
+                return type.GetProperty(fallbackProperty);
+            }
+
+            return property;
+        }
+    }
+}
