714859e189cc8cd10970917010d140e4d055621d
YouJinTou <dandreevd@gmail.com>
2019-08-07 19:59:10 +0300

    Added some constants.

--- BeachRankings.Core/Constants.cs
+++ BeachRankings.Core/Constants.cs
@@ -8,5 +8,11 @@
             public const string AwsAccessToken = "AWS_ACCESS_TOKEN";
             public const string AwsSecret = "AWS_SECRET";
         }
+
+        public static class View
+        {
+            public const string Ascending = "ASC";
+            public const string Descending = "DESC";
+        }
     }
 }
