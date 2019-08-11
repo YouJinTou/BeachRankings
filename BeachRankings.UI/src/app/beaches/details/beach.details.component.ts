4ca9cee716de671bbf148ff75d73dc94784f3b5c
YouJinTou <dandreevd@gmail.com>
2019-08-11 10:14:09 +0300

    Implemented pinging the API for the beach details.

--- BeachRankings.UI/src/app/beaches/details/beach.details.component.ts
+++ BeachRankings.UI/src/app/beaches/details/beach.details.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Beach } from 'src/app/models/search/beach';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'beach-details',
   templateUrl: './beach.details.component.html',
   styleUrls: ['./beach.details.component.css']
 })
-export class BeachDetailsComponent {
+export class BeachDetailsComponent implements OnInit {
+  beach: Beach;
+
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.httpClient.get<Beach>(environment.beachesEndpoint + params['id'])
+        .subscribe(beach => this.beach = beach);
+    });
+  }
 }
