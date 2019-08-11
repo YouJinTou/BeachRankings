76d79c767971876eb951d873c1a0beb67c8188f1
YouJinTou <dandreevd@gmail.com>
2019-08-11 09:25:05 +0300

    Added BeachDetailsComponent.

---
+++ BeachRankings.UI/src/app/beaches/beach.details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'beach-details',
+  templateUrl: './beach.details.component.html',
+  styleUrls: ['./beach.details.component.css']
+})
+export class BeachDetailsComponent {
+}
