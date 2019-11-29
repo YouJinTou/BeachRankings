1f535d9cefb56c0b7fdbc6157b6a086555198132
YouJinTou <dandreevd@gmail.com>
2019-11-29 18:06:28 +0200

    Added IEventStore.

---
+++ BR.Core/Abstractions/IEventStore.cs
@@ -0,0 +1,11 @@
+﻿using BR.Core.Events;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BR.Core.Abstractions
+{
+    public interface IEventStore
+    {
+        Task AddEventsAsync(IEnumerable<EventBase> events);
+    }
+}
