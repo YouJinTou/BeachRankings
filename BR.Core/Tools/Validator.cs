54cdc6b3a044bc09802af48da5c7d9c48eb789f5
YouJinTou <dandreevd@gmail.com>
2019-11-29 19:07:36 +0200

    Added more methods.

--- BR.Core/Tools/Validator.cs
+++ BR.Core/Tools/Validator.cs
@@ -4,12 +4,30 @@ namespace BR.Core.Tools
 {
     public static class Validator
     {
-        public static void ThrowIfNull(object obj)
+        public static void ThrowIfNull(object obj, string message = null)
         {
             if (obj == null)
             {
-                throw new ArgumentNullException(nameof(obj), "Parameter is null.");
+                throw new ArgumentNullException(nameof(obj), message ?? "Parameter is null.");
             }
+        }
+
+        public static void ThrowIfNullOrWhiteSpace<T>(T obj, string message = null)
+        {
+            ThrowIfNull(obj, message);
+
+            if (obj is string && string.IsNullOrWhiteSpace(obj as string))
+            {
+                throw new ArgumentNullException(
+                    nameof(obj), "String is null or white space." ?? message);
+            }
+        }
+
+        public static T ReturnOrThrowIfNullOrWhiteSpace<T>(T obj)
+        {
+            ThrowIfNullOrWhiteSpace(obj);
+
+            return obj;
         }
     }
 }
