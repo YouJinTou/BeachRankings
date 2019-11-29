5637cc9dcaae797e112c1ebcc429814e6db45b61
YouJinTou <dandreevd@gmail.com>
2019-11-29 18:49:06 +0200

    Added Validator.

---
+++ BR.Core/Tools/Validator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BR.Core.Tools
+{
+    public static class Validator
+    {
+        public static void ThrowIfNull(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj), "Parameter is null.");
+            }
+        }
+    }
+}
