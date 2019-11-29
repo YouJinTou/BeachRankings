c0c40ae1abbd0eb0594032178b11cd17b75b334e
YouJinTou <dandreevd@gmail.com>
2019-11-29 18:08:11 +0200

    Added UserModified.

---
+++ BR.Core/Events/UserModified.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BR.Core.Events
+{
+    public class UserModified : EventBase
+    {
+        public UserModified(Guid id, int offset) 
+            : base(id, offset)
+        {
+        }
+    }
+}
