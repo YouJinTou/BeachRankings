5ec2afb646bd6f771325bcb80a5c0fa55a23605d
YouJinTou <dandreevd@gmail.com>
2019-11-29 18:49:50 +0200

    Added UsersService.

---
+++ BR.UsersService/Abstractions/IUsersService.cs
@@ -0,0 +1,10 @@
+﻿using BR.UsersService.Models;
+using System.Threading.Tasks;
+
+namespace BR.UsersService.Abstractions
+{
+    internal interface IUsersService
+    {
+        Task CreateUserAsync(CreateUserModel model);
+    }
+}
---
+++ BR.UsersService/Services/UsersService.cs
@@ -0,0 +1,36 @@
+﻿using BR.Core.Abstractions;
+using BR.Core.Tools;
+using BR.UsersService.Abstractions;
+using BR.UsersService.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace BR.UsersService.Services
+{
+    internal class UsersService : IUsersService
+    {
+        private readonly IEventStore store;
+        private readonly ILogger<UsersService> logger;
+
+        public UsersService(IEventStore store, ILogger<UsersService> logger)
+        {
+            this.store = store;
+            this.logger = logger;
+        }
+
+        public async Task CreateUserAsync(CreateUserModel model)
+        {
+            try
+            {
+                Validator.ThrowIfNull(model);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, $"Failed to create user {model?.Username}.");
+
+                throw;
+            }
+        }
+    }
+}
