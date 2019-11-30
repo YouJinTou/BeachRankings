ecab609d0427e1c8bce5e6ac6c572930f44865cd
YouJinTou <dandreevd@gmail.com>
2019-11-30 17:54:24 +0200

    Added EnumerableExtensions.

---
+++ BR.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BR.Core.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable.IsNull())
+            {
+                return true;
+            }
+
+            return enumerable.Count() == 0;
+        }
+    }
+}
