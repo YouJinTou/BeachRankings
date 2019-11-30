afd3b809ca884f40e36d36d068fc708eb2026d19
YouJinTou <dandreevd@gmail.com>
2019-11-30 17:54:32 +0200

    Added ObjectExtensions.

---
+++ BR.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BR.Core.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsNull(this object obj)
+        {
+            return obj == null;
+        }
+
+        public static IEnumerable<T> AsEnumerable<T>(this T obj)
+        {
+            return new List<T> { obj };
+        }
+    }
+}
