3dbe703b8e59b640624cb4c91990393714bae438
YouJinTou <dandreevd@gmail.com>
2019-12-01 16:52:07 +0200

    Added Beach_GetBeachModel.

---
+++ BR.BeachesService/Profiles/Beach_GetBeachModel.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using BR.BeachesService.Models;
+
+namespace BR.BeachesService.Profiles
+{
+    public class Beach_GetBeachModel : Profile
+    {
+        public Beach_GetBeachModel()
+        {
+            this.CreateMap<Beach, GetBeachModel>();
+        }
+    }
+}
