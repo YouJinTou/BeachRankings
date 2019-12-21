cf51dfa6786d89b16478f59184f43f7f465d6102
YouJinTou <dandreevd@gmail.com>
2019-12-21 13:03:11 +0200

    Minor formatting changes.

--- BR.Iam/Services/AuthService.cs
+++ BR.Iam/Services/AuthService.cs
@@ -66,7 +66,8 @@ namespace BR.Iam.Services
                 var accessTokensMatch =
                     !string.IsNullOrWhiteSpace(user.AccessToken)
                     && user.AccessToken == model.AccessToken;
-                var expirationDateValid = user.AccessTokenExpiresAt.HasValue 
+                var expirationDateValid = 
+                    user.AccessTokenExpiresAt.HasValue 
                     && user.AccessTokenExpiresAt > DateTime.UtcNow;
                 var result = new AuthResult
                 {
