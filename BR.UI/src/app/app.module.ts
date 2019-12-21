05072680dd0442c83a2b739cae17529c121b35a9
YouJinTou <dandreevd@gmail.com>
2019-12-21 14:04:09 +0200

    Added header and routing.

--- BR.UI/src/app/app.module.ts
+++ BR.UI/src/app/app.module.ts
@@ -2,20 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CreateBeachComponent } from './components/create-beach/create-beach.component';
 import { LoginComponent } from './components/login/login.component';
 import { CookieService } from 'ngx-cookie-service';
+import { HeaderComponent } from './components/header/header.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'add-beach', component: CreateBeachComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateBeachComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes, {
+      enableTracing: false
+    }),
     FormsModule,
     HttpClientModule
   ],
