fdd1223cf8e66235d342994fa0793b817cf163b8
YouJinTou <dandreevd@gmail.com>
2019-12-22 14:22:19 +0200

    Added Place.

---
+++ BR.Core/Models/Place.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using BR.Core.Abstractions;
+using System.Collections.Generic;
+
+namespace BR.Core.Models
+{
+    public class Place : IDbModel
+    {
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public IEnumerable<string> Children { get; set; }
+
+        public string Continent { get; set; }
+
+        public string Country { get; set; }
+
+        public string L1 { get; set; }
+
+        public string L2 { get; set; }
+
+        public string L3 { get; set; }
+
+        public string L4 { get; set; }
+
+        public IEnumerable<string> WaterBodies { get; set; }
+    }
+}
---
+++ BR.Core/Models/PlaceType.cs
@@ -0,0 +1,14 @@
+﻿namespace BR.Core.Models
+{
+    public enum PlaceType
+    {
+        Continent,
+        Country,
+        L1,
+        L2,
+        L3,
+        L4,
+        WaterBody,
+        Beach
+    }
+}
