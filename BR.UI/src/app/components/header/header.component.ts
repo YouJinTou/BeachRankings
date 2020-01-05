3e85d2b8078a9a16597222bc760ec615d39c5013
YouJinTou <dandreevd@gmail.com>
2020-01-05 17:27:37 +0200

    Added some CSS.

--- BR.UI/src/app/components/header/header.component.ts
+++ BR.UI/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/services/users.service';
+import { LoginResult } from 'src/app/models/login.result';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,14 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class HeaderComponent implements OnInit {
   isLoggedIn: boolean;
+  user: LoginResult;
 
   constructor(private usersService: UsersService) { }
 
   ngOnInit() {
     this.usersService.isAuthenticated().subscribe(r => this.isLoggedIn = r);
+
+    this.usersService.currentUser.subscribe(r => this.user = r);
   }
 
   logout() {
