f35fdd284c82c743b589f6c1d9dd58c75e612dbc
YouJinTou <dandreevd@gmail.com>
2021-01-05 00:13:55 +0200

    Fixed compile-time errors.

--- FSharpPort/BR.Core/Library.fs
+++ FSharpPort/BR.Core/Library.fs
@@ -18,75 +18,85 @@ module Score =
         | Walking of float
         | Camping of float
         | LongTermStay of float
-    type T = private {
-        Average: V option
-        SandQuality: V option
-        BeachCleanliness: V option
-        BeautifulScenery: V option
-        CrowdFree: V option
-        Infrastructure: V option
-        WaterVisibility: V option
-        LitterFree: V option
-        FeetFriendlyBottom: V option
-        SeaLifeDiversity: V option
-        CoralReef: V option
-        Snorkeling: V option
-        Kayaking: V option
-        Walking: V option
-        Camping: V option
-        LongTermStay: V option
-    }
-    let empty = {
-        Average = None
-        SandQuality = None
-        BeachCleanliness = None
-        BeautifulScenery = None
-        CrowdFree = None
-        Infrastructure = None
-        WaterVisibility = None
-        LitterFree = None
-        FeetFriendlyBottom = None
-        SeaLifeDiversity = None
-        CoralReef = None
-        Snorkeling = None
-        Kayaking = None
-        Walking = None
-        Camping = None
-        LongTermStay = None
-    }
+
+    type T =
+        private
+            { Average: V option
+              SandQuality: V option
+              BeachCleanliness: V option
+              BeautifulScenery: V option
+              CrowdFree: V option
+              Infrastructure: V option
+              WaterVisibility: V option
+              LitterFree: V option
+              FeetFriendlyBottom: V option
+              SeaLifeDiversity: V option
+              CoralReef: V option
+              Snorkeling: V option
+              Kayaking: V option
+              Walking: V option
+              Camping: V option
+              LongTermStay: V option }
+
+    let empty =
+        { Average = None
+          SandQuality = None
+          BeachCleanliness = None
+          BeautifulScenery = None
+          CrowdFree = None
+          Infrastructure = None
+          WaterVisibility = None
+          LitterFree = None
+          FeetFriendlyBottom = None
+          SeaLifeDiversity = None
+          CoralReef = None
+          Snorkeling = None
+          Kayaking = None
+          Walking = None
+          Camping = None
+          LongTermStay = None }
+
     let create scores =
         let t = empty
-        let av c (l: int) (v: float) =
-            match c with
-                | Some currVal -> (currVal + v) / (l |> float)
-                | None _ -> v
+
+        let av acc (len: int) (v: float) =
+            match acc with
+            | Some f -> Some ((f + v) / (len |> float))
+            | None _ -> Some v
+        let average x = 
+            match x with
+            | Some v -> Some (Average v)
+            | None _ -> None
+
         let rec assign scores acc avg =
             let len = List.length scores
             let av2 = av avg len
+
             match scores with
-            | [] -> { acc with Average = avg }
-            | head :: tail -> 
+            | [] -> { acc with Average = average avg }
+            | head :: tail ->
                 match head with
-                    | Average _ -> assign tail t avg
-                    | SandQuality v -> assign tail { t with SandQuality = Some head } (av2 v)
-                    | BeachCleanliness _ -> assign tail { t with BeachCleanliness = Some head } 
-                    | BeautifulScenery _ -> assign tail { t with BeautifulScenery = Some head } 
-                    | CrowdFree _ -> assign tail { t with CrowdFree = Some head } 
-                    | Infrastructure _ -> assign tail { t with Infrastructure = Some head } 
-                    | WaterVisibility _ -> assign tail { t with WaterVisibility = Some head } 
-                    | LitterFree _ -> assign tail { t with LitterFree = Some head } 
-                    | FeetFriendlyBottom _ -> assign tail { t with FeetFriendlyBottom = Some head } 
-                    | SeaLifeDiversity _ -> assign tail { t with SeaLifeDiversity = Some head } 
-                    | CoralReef _ -> assign tail { t with CoralReef = Some head } 
-                    | Snorkeling _ -> assign tail { t with Snorkeling = Some head } 
-                    | Kayaking _ -> assign tail { t with Kayaking = Some head } 
-                    | Walking _ -> assign tail { t with Walking = Some head } 
-                    | Camping _ -> assign tail { t with Camping = Some head } 
-                    | LongTermStay _ -> assign tail { t with LongTermStay = Some head } 
+                | Average _ -> assign tail t avg
+                | SandQuality v -> assign tail { t with SandQuality = Some head } (av2 v)
+                | BeachCleanliness v -> assign tail { t with BeachCleanliness = Some head } (av2 v)
+                | BeautifulScenery v -> assign tail { t with BeautifulScenery = Some head } (av2 v)
+                | CrowdFree v -> assign tail { t with CrowdFree = Some head } (av2 v)
+                | Infrastructure v -> assign tail { t with Infrastructure = Some head } (av2 v)
+                | WaterVisibility v -> assign tail { t with WaterVisibility = Some head } (av2 v)
+                | LitterFree v -> assign tail { t with LitterFree = Some head } (av2 v)
+                | FeetFriendlyBottom v -> assign tail { t with FeetFriendlyBottom = Some head } (av2 v)
+                | SeaLifeDiversity v -> assign tail { t with SeaLifeDiversity = Some head } (av2 v)
+                | CoralReef v -> assign tail { t with CoralReef = Some head } (av2 v)
+                | Snorkeling v -> assign tail { t with Snorkeling = Some head } (av2 v)
+                | Kayaking v -> assign tail { t with Kayaking = Some head } (av2 v)
+                | Walking v -> assign tail { t with Walking = Some head } (av2 v)
+                | Camping v -> assign tail { t with Camping = Some head } (av2 v)
+                | LongTermStay v -> assign tail { t with LongTermStay = Some head } (av2 v)
+
         let result = assign scores t None
         result
 
-module Place = 
+module Place =
     type Continent = Continent of string
     type WaterBody = WaterBody of string
     type Country = Country of string option
@@ -94,17 +104,17 @@ module Place =
     type L2 = L2 of string option
     type L3 = L3 of string option
     type L4 = L4 of string option
-    type T = {
-        Continent: Continent
-        WaterBody: WaterBody
-        Country: Country
-        L1: L1
-        L2: L2
-        L3: L3
-        L4: L4
-    }
 
-// module Beach = 
+    type T =
+        { Continent: Continent
+          WaterBody: WaterBody
+          Country: Country
+          L1: L1
+          L2: L2
+          L3: L3
+          L4: L4 }
+
+// module Beach =
 //     open System
 
 //     type Name = Name of string
@@ -118,7 +128,7 @@ module Place =
 //         AddedBy: Guid
 //         CreatedAt: DateTime
 //     }
-//     let create name place coords score addedBy = 
+//     let create name place coords score addedBy =
 //         {
 //             Id = Guid.NewGuid()
 //             Place = place
